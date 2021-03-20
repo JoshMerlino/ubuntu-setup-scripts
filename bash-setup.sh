6ba24109e4576554886725e1443dc25774e4f357
Josh Merlino <31785395+JoshMerlino@users.noreply.github.com>
2021-03-20 00:54:19 -0400

    Update bash-setup.sh

--- bash-setup.sh
+++ bash-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 username=$1
 
 # Make sure user exists
@@ -16,20 +15,20 @@ usermod -a -G nopasswd $username
 echo "%nopasswd ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nopasswd
 
 # Set up color prompt
-echo "PS1='\e[30;44m \h \e[0m\e[34;104m\e[0m\e[0m\e[30;104m \u \e[0m\e[94;107m\e[0m\e[0m\e[30;107m \w \e[0m\e[97m\e[0m '" > /etc/profile.d/bash_prompt.sh
+echo "PS1='\e[30;44m \h \e[0m\e[34;104m\e[0m\e[0m\e[30;104m \u \e[0m\e[94;107m\e[0m\e[0m\e[30;107m \w \e[0m\e[97m\e[0m '" > /etc/profile
 
 # Set up aliases
-echo "" > /etc/profile.d/bash_aliases.sh
-echo "alias ls='ls --color=auto'" >> /etc/profile.d/bash_aliases.sh
-echo "alias grep='grep --color=auto'" >> /etc/profile.d/bash_aliases.sh
-echo "alias fgrep='fgrep --color=auto'" >> /etc/profile.d/bash_aliases.sh
-echo "alias egrep='egrep --color=auto'" >> /etc/profile.d/bash_aliases.sh
-echo "alias ll='ls -alF'" >> /etc/profile.d/bash_aliases.sh
-echo "alias la='ls -A'" >> /etc/profile.d/bash_aliases.sh
-echo "alias l='ls -CF'" >> /etc/profile.d/bash_aliases.sh
+echo "" > /etc/profile
+echo "alias ls='ls --color=auto'" >> /etc/profile
+echo "alias grep='grep --color=auto'" >> /etc/profile
+echo "alias fgrep='fgrep --color=auto'" >> /etc/profile
+echo "alias egrep='egrep --color=auto'" >> /etc/profile
+echo "alias ll='ls -alF'" >> /etc/profile
+echo "alias la='ls -A'" >> /etc/profile
+echo "alias l='ls -CF'" >> /etc/profile
 
 # If wsl setup
-if grep -q microsoft /proc/version; then echo "alias reboot=\"echo \\\"Run 'Get-Service LxssManager | Restart-Service' as administrator in PowerShell.\\\"\"" >> /etc/profile.d/bash_aliases.sh; fi
+if grep -q microsoft /proc/version; then echo "alias reboot=\"echo \\\"Run 'Get-Service LxssManager | Restart-Service' as administrator in PowerShell.\\\"\"" >> /etc/profile; fi
 
 
 # Link root to home dir
